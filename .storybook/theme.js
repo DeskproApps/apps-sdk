abf4b4ba340d076ab6d7400f2819e23be05b4926
ITweeno <itweeno@gmail.com>
2023-03-21 17:49:56 +0000

    url change

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,7 +3,8 @@ import { create } from "@storybook/theming";
 export default create({
   base: "light",
   brandImage: "https://i.ibb.co/DLZ51FT/DP-logo.png",
-  brandUrl: "https://www.deskpro.com/",
+  brandUrl:
+    "https://support.deskpro.com/en-US/guides/developers/how-apps-extend-deskpro",
   brandTitle: "Deskpro",
   brandTarget: "_self",
 });
