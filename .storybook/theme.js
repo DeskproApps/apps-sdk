0c8105028e4c5119cf9871b16a1a199c5f981214
ITweeno <itweeno@gmail.com>
2023-03-21 17:45:23 +0000

    asdf

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,9 +2,8 @@ import { create } from "@storybook/theming";
 
 export default create({
   base: "light",
-  brandImage:
-    "https://i.kym-cdn.com/entries/icons/mobile/000/031/003/cover3.jpg",
-  brandUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  brandImage: "https://i.ibb.co/DLZ51FT/DP-logo.png",
+  brandUrl: "https://www.deskpro.com/",
   brandTitle: "Deskpro",
   brandTarget: "_self",
 });
