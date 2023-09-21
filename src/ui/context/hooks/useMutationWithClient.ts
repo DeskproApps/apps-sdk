98119296aaab19a3b001de32dcd0bca2ea515eff
ilia makarov <ilia.makarov@me.com>
2023-09-21 17:09:48 +0300

    Pass options to the useMutationsWithClient

--- src/ui/context/hooks/useMutationWithClient.ts
+++ src/ui/context/hooks/useMutationWithClient.ts
@@ -1,17 +1,18 @@
 import { useMutation } from "@tanstack/react-query";
 import { useDeskproAppClient } from "./useDeskproAppClient";
-import { IDeskproClient } from "../../../client/types";
+import type { UseMutationOptions } from "@tanstack/react-query";
+import type { IDeskproClient } from "../../../client/types";
 
 export const useMutationWithClient = <TFuncParams = unknown, TData = unknown>(
-  queryFn: (client: IDeskproClient, data: TFuncParams) => Promise<TData>
+  queryFn: (client: IDeskproClient, data: TFuncParams) => Promise<TData>,
+  options?:
+    | Omit<UseMutationOptions<TData, unknown, unknown, unknown>, "mutationFn">
+    | undefined
 ) => {
   const { client } = useDeskproAppClient();
 
-  return useMutation<TData, unknown, unknown, unknown>((data) => {
-    if (!client) {
-      throw new Error("Client is not initialised");
-    }
-
-    return queryFn(client, data as TFuncParams);
-  });
+  return useMutation<TData, unknown, unknown, unknown>(
+    (data) => (!client ? null : queryFn(client, data as TFuncParams)) as ReturnType<typeof queryFn>,
+    options,
+  );
 };
