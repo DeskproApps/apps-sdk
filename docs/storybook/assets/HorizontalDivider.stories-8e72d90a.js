0c62ce24cb6174ae95e3df8490e5eebad2237bac
git log -1 --pretty=format:%an <ashley.dawson@deskpro.com>
2023-09-25 14:38:00 +0000

    Updated Storybook

---
+++ docs/storybook/assets/HorizontalDivider.stories-8e72d90a.js
@@ -0,0 +1,4 @@
+import{j as o}from"./jsx-runtime-29545a09.js";import{E as p}from"./SPA-3e501c95.js";import"./index-76fb7be0.js";import"./index-d3ea75b5.js";import{D as s}from"./DeskproAppProvider-c48c9b34.js";import{H as m}from"./Divider-6a189474.js";import"./_commonjsHelpers-de833af9.js";import"./index-9d475cdf.js";const H={title:"Core/Divider"},r=()=>o(s,{theme:p,children:o(m,{width:2})});var e,i,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`() => <DeskproAppProvider theme={lightTheme}>
+    <HorizontalDividerComp width={2} />
+  </DeskproAppProvider>`,...(t=(i=r.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const f=["HorizontalDividerStory"];export{r as HorizontalDividerStory,f as __namedExportsOrder,H as default};
+//# sourceMappingURL=HorizontalDivider.stories-8e72d90a.js.map
---
+++ docs/storybook/assets/Section.stories-f6c196a2.js
@@ -0,0 +1,6 @@
+import{j as n}from"./jsx-runtime-29545a09.js";import{S as s}from"./Section-162658f4.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";const m={title:"Core/Section"},e=()=>n(s,{children:"App Section Content"});var t,o,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`() => {
+  return <Section>
+      App Section Content
+    </Section>;
+}`,...(r=(o=e.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const S=["Default"];export{e as Default,S as __namedExportsOrder,m as default};
+//# sourceMappingURL=Section.stories-f6c196a2.js.map
