0e71c999e660238e225f9cb3e730219eb284c95e
git log -1 --pretty=format:%an <ashley.dawson@deskpro.com>
2023-11-14 13:17:05 +0000

    Updated Storybook

--- docs/storybook/assets/VerticalDivider.stories-39a8a2e1.js
+++
@@ -1,6 +0,0 @@
-import{j as e}from"./jsx-runtime-29545a09.js";import{E as s}from"./SPA-9917dd72.js";import"./index-76fb7be0.js";import"./index-d3ea75b5.js";import{D as p}from"./DeskproAppProvider-edca97d3.js";import{V as m}from"./Divider-46ad874b.js";import"./_commonjsHelpers-de833af9.js";import"./index-9d475cdf.js";const V={title:"Core/Divider"},r=()=>e(p,{theme:s,children:e(m,{width:2,style:{height:"100vh"}})});var i,t,o;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`() => <DeskproAppProvider theme={lightTheme}>
-    <VerticalDividerComp width={2} style={{
-    height: "100vh"
-  }} />
-  </DeskproAppProvider>`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const f=["VerticalDividerStory"];export{r as VerticalDividerStory,f as __namedExportsOrder,V as default};
-//# sourceMappingURL=VerticalDivider.stories-39a8a2e1.js.map
