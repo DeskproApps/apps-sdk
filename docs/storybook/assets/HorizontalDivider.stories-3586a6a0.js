30db3c7f5b87403022da33207cda03eb93f76f9e
git log -1 --pretty=format:%an <ashley.dawson@deskpro.com>
2024-03-06 16:28:58 +0000

    Updated Storybook

--- docs/storybook/assets/HorizontalDivider.stories-3586a6a0.js
+++
@@ -1,3 +0,0 @@
-import{j as o}from"./jsx-runtime-29545a09.js";import{l as p}from"./SPA-7e65fab1.js";import"./index-76fb7be0.js";import"./index-d3ea75b5.js";import{D as s}from"./DeskproAppProvider-c322f515.js";import{H as m}from"./Divider-e9dce165.js";import"./_commonjsHelpers-de833af9.js";import"./index-9d475cdf.js";const H={title:"Core/Divider"},r=()=>o(s,{theme:p,children:o(m,{width:2})});var e,i,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`() => <DeskproAppProvider theme={lightTheme}>
-    <HorizontalDividerComp width={2} />
-  </DeskproAppProvider>`,...(t=(i=r.parameters)==null?void 0:i.docs)==null?void 0:t.source}}};const f=["HorizontalDividerStory"];export{r as HorizontalDividerStory,f as __namedExportsOrder,H as default};
