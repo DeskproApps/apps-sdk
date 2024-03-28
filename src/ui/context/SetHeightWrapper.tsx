c88d0b174524974c093f9daf0d4e660ad879eac0
Jeremy Christian <jeremy.christian@deskpro.com>
2024-03-28 17:27:23 +0000

    resolve margin issue

--- src/ui/context/SetHeightWrapper.tsx
+++ src/ui/context/SetHeightWrapper.tsx
@@ -5,16 +5,19 @@ export const HeightWrapper = ({ children }: { children: React.ReactNode }) => {
   const { client } = useDeskproAppClient();
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    if (!ref.current) return;
-    const body = document.body;
-    const styles = window.getComputedStyle(body);
-    const marginTop = styles.getPropertyValue("margin-top");
-    const marginBottom = styles.getPropertyValue("margin-bottom");
-    const additionalHeight = parseInt(marginTop) ?? 0 + parseInt(marginBottom) ?? 0;
+    const container = ref.current;
+    if (!container) return;
     const resizeObserver = new ResizeObserver(() => {
-      client?.setHeight(document.body.scrollHeight + additionalHeight);
+      const child = container?.children[0];
+      if (child) {
+        const styles = window.getComputedStyle(child);
+        const marginTop = styles.getPropertyValue("margin-top");
+        const marginBottom = styles.getPropertyValue("margin-bottom");
+        const additionalHeight = (parseInt(marginTop) ?? 0) + (parseInt(marginBottom) ?? 0);
+        client?.setHeight(container.scrollHeight + additionalHeight);
+      }
     });
-    resizeObserver.observe(ref.current);
+    resizeObserver.observe(container);
     return () => resizeObserver.disconnect(); // clean up
   }, [client]);
   return <div ref={ref}>{children}</div>;
