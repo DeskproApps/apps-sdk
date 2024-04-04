9a46e2d1ec1cd24bcf391f33a7b7d7976fdde6de
Jeremy Christian <jeremy.christian@deskpro.com>
2024-04-04 16:35:00 +0100

    include body margin

--- src/ui/context/SetHeightWrapper.tsx
+++ src/ui/context/SetHeightWrapper.tsx
@@ -1,20 +1,30 @@
 import { useDeskproAppClient } from "./hooks";
 import React, { useEffect, useRef } from "react";
 
+const findAdditionalHeight = (styles: CSSStyleDeclaration) => {
+  const marginTop = styles.getPropertyValue("margin-top");
+  const marginBottom = styles.getPropertyValue("margin-bottom");
+
+  return (parseInt(marginTop) ?? 0) + (parseInt(marginBottom) ?? 0);
+};
+
 export const HeightWrapper = ({ children }: { children: React.ReactNode }) => {
   const { client } = useDeskproAppClient();
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    const container = ref.current;
+    const container = ref?.current;
     if (!container) return;
     const resizeObserver = new ResizeObserver(() => {
       const child = container?.children[0];
       if (child) {
-        const styles = window.getComputedStyle(child);
-        const marginTop = styles.getPropertyValue("margin-top");
-        const marginBottom = styles.getPropertyValue("margin-bottom");
-        const additionalHeight = (parseInt(marginTop) ?? 0) + (parseInt(marginBottom) ?? 0);
-        client?.setHeight(container.scrollHeight + additionalHeight);
+        const childStyles = window.getComputedStyle(child);
+        const bodyStyles = window.getComputedStyle(document.body);
+
+        client?.setHeight(
+          container.scrollHeight +
+            findAdditionalHeight(childStyles) +
+            findAdditionalHeight(bodyStyles)
+        );
       }
     });
     resizeObserver.observe(container);
