373f0e6c03f0269a73bd0c62a31af751fdb45a09
git log -1 --pretty=format:%an <1740195+HappyPaul55@users.noreply.github.com>
2024-11-07 11:02:39 +0000

    Updated Storybook

--- docs/storybook/assets/Section-162658f4.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./jsx-runtime-29545a09.js";const o=({children:e})=>t("div",{style:{padding:"10px 10px 0"},children:e});try{o.displayName="Section",o.__docgenInfo={description:"",displayName:"Section",props:{}}}catch{}export{o as S};
---
+++ docs/storybook/assets/TwoButtonGroup-a57853f8.js
@@ -0,0 +1,15 @@
+import{j as n}from"./jsx-runtime-d3c0a380.js";import{s as r,S as d,B as p}from"./SPA-d2c3e53d.js";import"./index-070054a4.js";import"./index-f347cf61.js";const m=r(d)`
+    margin-bottom: 10px;
+    padding: 6px 6px 7px;
+    border-radius: 6px;
+    background-color: ${({theme:e})=>e.colors.grey10};
+`,t=r(p)`
+    flex-grow: 1;
+    justify-content: center;
+
+    ${({selected:e})=>e?"":`
+        background-color: transparent;
+        border-color: transparent;
+        box-shadow: none;
+    `}
+`,o=({selected:e,oneLabel:a,twoLabel:i,oneOnClick:l,twoOnClick:u,oneIcon:c,twoIcon:s})=>n.jsxs(m,{justify:"space-between",align:"center",gap:6,children:[n.jsx(t,{text:a,intent:"secondary",icon:c,size:"large",selected:e==="one",onClick:l}),n.jsx(t,{text:i,intent:"secondary",icon:s,size:"large",selected:e==="two",onClick:u})]});try{o.displayName="TwoButtonGroup",o.__docgenInfo={description:"",displayName:"TwoButtonGroup",props:{selected:{defaultValue:null,description:"",name:"selected",required:!0,type:{name:"enum",value:[{value:'"one"'},{value:'"two"'}]}},oneLabel:{defaultValue:null,description:"",name:"oneLabel",required:!0,type:{name:"string"}},twoLabel:{defaultValue:null,description:"",name:"twoLabel",required:!0,type:{name:"string"}},oneOnClick:{defaultValue:null,description:"",name:"oneOnClick",required:!0,type:{name:"() => void"}},twoOnClick:{defaultValue:null,description:"",name:"twoOnClick",required:!0,type:{name:"() => void"}},oneIcon:{defaultValue:null,description:"",name:"oneIcon",required:!1,type:{name:"IconDefinition"}},twoIcon:{defaultValue:null,description:"",name:"twoIcon",required:!1,type:{name:"IconDefinition"}}}}}catch{}export{o as T};
