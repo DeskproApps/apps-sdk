047cff55953b782718091471753920105ca37ed1
git log -1 --pretty=format:%an <1740195+HappyPaul55@users.noreply.github.com>
2024-11-07 11:29:02 +0000

    Updated Storybook

--- docs/storybook/assets/preview-47a92a7c.js
+++
@@ -1,8 +0,0 @@
-import{j as r}from"./jsx-runtime-d3c0a380.js";import{D as s}from"./DeskproAppProvider-ebcc82c4.js";import"./CopyToClipboardInput-eaa31c41.js";import"./Divider-051964a7.js";import"./ExternalIconLink-0e58eb61.js";import"./ObservedDiv-c966652a.js";import"./Property-2de360cf.js";import"./PropertyRow-7ea313cd.js";import"./TwoProperties-7975c3aa.js";import"./Section-f8bb3afc.js";import{Z as i,_ as e,$ as a,a0 as m,a1 as d,a2 as n,s as p}from"./SPA-d2c3e53d.js";import"./index-070054a4.js";import"./index-f347cf61.js";import"./Title-bdb34d07.js";import"./TwoButtonGroup-a57853f8.js";import"./DateInput-c1136179.js";import"./Search-9e51d775.js";import"./Select-44546235.js";import"./Link-51a675e0.js";import"./Member-924b3965.js";import"./index-2476389d.js";import"./index-737698a4.js";const t={Container:i,Header:e,SubHeader:a,InputWrapper:m,Body:d,Footer:n};const c=p.div`
-  width: 300px;
-  height: 500px;
-  border: 1px dashed #D3D6D7;
-  border-radius: 3px;
-`,x=p.div`
-  margin: 8px;
-`,l=({children:o})=>r.jsx(c,{children:r.jsxs(t.Container,{children:[r.jsx(t.Header,{title:"Some App"}),r.jsx(t.Body,{children:r.jsx(x,{children:o})})]})}),z={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}},decorators:[o=>r.jsx(l,{children:r.jsx(o,{})}),o=>r.jsx(s,{children:r.jsx(o,{})})]};export{z as default};
