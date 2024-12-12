5cdb1ad840a8644c0151178593025121ca14019b
git log -1 --pretty=format:%an <1740195+HappyPaul55@users.noreply.github.com>
2024-12-12 10:19:26 +0000

    Updated Storybook

--- docs/storybook/assets/VerticalDivider.stories-de38bd70.js
+++
@@ -1,5 +0,0 @@
-import{j as e}from"./jsx-runtime-d3c0a380.js";import{l as s}from"./SPA-bfde073d.js";import"./index-070054a4.js";import"./index-f347cf61.js";import{D as p}from"./DeskproAppProvider-337b6592.js";import{V as m}from"./Divider-efb6696f.js";import"./index-737698a4.js";const D={title:"Core/Divider"},r=()=>e.jsx(p,{theme:s,children:e.jsx(m,{width:2,style:{height:"100vh"}})});var i,t,o;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`() => <DeskproAppProvider theme={lightTheme}>
-    <VerticalDividerComp width={2} style={{
-    height: "100vh"
-  }} />
-  </DeskproAppProvider>`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const x=["VerticalDividerStory"];export{r as VerticalDividerStory,x as __namedExportsOrder,D as default};
---
+++ docs/storybook/assets/preview-d0fb8ec9.js
@@ -0,0 +1,8 @@
+import{j as r}from"./jsx-runtime-d3c0a380.js";import{D as s}from"./DeskproAppProvider-69d1de02.js";import"./CopyToClipboardInput-24aaaef3.js";import"./Divider-efb6696f.js";import"./ExternalIconLink-0478d2ce.js";import"./ObservedDiv-2547ed84.js";import"./Property-7b0e46ae.js";import"./PropertyRow-a14ce6e3.js";import"./TwoProperties-4a60fc34.js";import"./Section-f8bb3afc.js";import{Z as i,_ as e,$ as a,a0 as m,a1 as d,a2 as n,s as p}from"./SPA-bfde073d.js";import"./index-070054a4.js";import"./index-f347cf61.js";import"./Title-6f817085.js";import"./TwoButtonGroup-4ccffc9d.js";import"./DateInput-af7fbe20.js";import"./Search-12b40806.js";import"./Select-7fcd3c32.js";import"./Link-fdf0b569.js";import"./Member-5ce06275.js";import"./index-2476389d.js";import"./index-737698a4.js";const t={Container:i,Header:e,SubHeader:a,InputWrapper:m,Body:d,Footer:n};const c=p.div`
+  width: 300px;
+  height: 500px;
+  border: 1px dashed #D3D6D7;
+  border-radius: 3px;
+`,x=p.div`
+  margin: 8px;
+`,l=({children:o})=>r.jsx(c,{children:r.jsxs(t.Container,{children:[r.jsx(t.Header,{title:"Some App"}),r.jsx(t.Body,{children:r.jsx(x,{children:o})})]})}),z={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}},decorators:[o=>r.jsx(l,{children:r.jsx(o,{})}),o=>r.jsx(s,{children:r.jsx(o,{})})]};export{z as default};
